1605bc28ab7074f0199d264f56b45942276996a5
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-10-09 19:45:59 -0300

    Inclusão de novas resoluções

--- ex053.py
+++ ex053.py
@@ -1,9 +1,8 @@
 # 53: Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
-from time import sleep
 frase = str(input('Digite uma frase qualquer: ')).lower().replace(" ","")
-lista_invertida = frase[::-1]
-print('De trás pra frente fica assim: {}'.format(lista_invertida))
-if frase == lista_invertida:
+frase_invertida = frase[::-1]
+print('De trás pra frente fica assim: {}'.format(frase_invertida))
+if frase == frase_invertida:
     print('E por isso, esta frase é um PALÍNDROMO!!!')
 else:
     print('E por isot, essa frase NÃO É UM PALÍNDROMO.')
--- ex054.py
+++ ex054.py
@@ -1,13 +1,14 @@
 # 54: Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
 from datetime import date
 ano_atual = date.today().year
-maioridade = []
+maioridade = 0
+menoridade = 0
 for i in range (0,7):
-    ano_nascimento = int(input('Escreva seu ano de nascimento: '))
+    ano_nascimento = int(input('{} Escreva seu ano de nascimento: '.format(i+1)))
     idade = ano_atual - ano_nascimento
     if idade >= 18:
-        maioridade.append(1)
-total_maioridade = sum(maioridade)
-total_menor = 7 - total_maioridade
-print('Nº de pessoas que já atingiu a maioridade: {}\nNº de pessoas que são "de menor": {}.'.format(total_maioridade,total_menor))
+        maioridade += 1
+    else:
+        menoridade += 1
+print('Nº de pessoas que já atingiu a maioridade: {}\nNº de pessoas que são "de menor": {}.'.format(maioridade,menoridade))
 
