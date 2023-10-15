0e174d1f1351fee249775a3830a0edb9f5991aa2
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-10-15 10:22:32 -0300

    Inclusão de novas resoluções

--- ex070.py
+++ ex070.py
@@ -2,27 +2,27 @@
 a) qual é o total gasto na compra;
 b) quantos produtos custam mais de R$1.000,00;
 c) qual é o nome do produto mais barato."""
-total_gasto = 0
-produtos_mais_1000 = 0
-produto_mais_barato = 0
-cont_prod_mais_1000 = 0
+total_gasto = produto_mais_barato = menor_preço = cont_prod_mais_1000 = 0
+continua = ' '
 while True:
-    produto = input('Nome do Produto: ')
-    preço = float(input('Preço do Produto: R$'))
-    continua = input('Deseja continuar? [S/N] ').lower().strip()
+    produto = input('Produto: ')
+    preço = float(input('Preço: R$'))
     total_gasto += preço
-    produto_mais_barato = produto
-    menor_preço = preço
     if preço > 1000:
-        produtos_mais_1000 += preço
         cont_prod_mais_1000 += 1
+    if menor_preço == 0:
+        menor_preço = preço
+        produto_mais_barato = produto
     if preço < menor_preço:
         menor_preço = preço
         produto_mais_barato = produto
-    if continua in 'sn':
-        if continua == 'n':
-            break
-    else:
-        continua = input('Deseja continuar? [S/N] ').lower().strip()
-print(f'O total gasto foi R${total_gasto:.2f}, a quantidade de itens acima de R$1.000,00 foi: {cont_prod_mais_1000} e o produto mais barato foi {produto_mais_barato}.')
+    continua = input('Deseja continuar? [S/N] ').lower().strip()[0]
+    while continua not in 'sn':
+        continua = input('Deseja continuar? [S/N] ').lower().strip()[0]
+    if continua in 'n':
+        break
+print('{:^60}'.format('PROGRAMA ENCERRADO'))
+print('-*'*30)
+print(f'\033[1;32mO total gasto foi R${total_gasto:.2f}\nA quantidade de itens acima de R$1.000,00 foi: {cont_prod_mais_1000}.\nO produto mais barato foi {produto_mais_barato}, que custa R${menor_preço:.2f}\033[m')
+print('-*'*30)
 
