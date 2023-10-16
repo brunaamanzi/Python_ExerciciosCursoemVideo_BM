7d9bf436fb2d2ab0bf06e2cdbeccad7c29ad2817
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-10-16 15:23:49 -0300

    Inclusão de novas resoluções

---
+++ ex073.py
@@ -0,0 +1,16 @@
+"""73: Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois, mostre:
+a) apenas os 5 primeiros colocados;
+b) os últimos 4 colocados da tabela;
+c) uma lista com os times em ordem alfabética;
+d) em que posição na tabela está o time da Chapecoense."""
+print('-'*50)
+print(f'\033[1;34m{"Tabela do Campeonato Brasileiro de Futebol":^49}\033[m')
+print('-'*50)
+times = ('Flamengo','Santos','Palmeiras','Gremio','Atletico Paranaense','São Paulo','Internacional','Conrithians','Fortaleza','Goias','Bahia','Vasco','Atletico Mineiro','Fluminense','Botafogo','Ceará','Cruzeiro','CSA','Chapecoense','Avaí')
+print(f'\033[1;34mOs 20 primeiros colocados:\033[m\n{times}')
+print(f'\033[1;34mOs 5 primeiros colocados:\033[m\n{times[0:5]}')
+print(f'\033[1;34mOs 4 últimos colocados:\033[m\n{times[16:]}')
+print(f'\033[1;34mRelação dos times em Ordem Alfabética:\033[m\n{sorted(times)}')
+print(f'\033[1;34mO Chapecoense está na {times.index("Chapecoense")+1}ª posição.\033[m')
+
+
---
+++ ex074.py
@@ -0,0 +1,14 @@
+# 74: Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
+from time import sleep
+print('Olá, vamos sortear 5 números entre 0 e 10...')
+sleep(1)
+from random import randint
+aleatório_1 = randint(0,10)
+aleatório_2 = randint(0,10)
+aleatório_3 = randint(0,10)
+aleatório_4 = randint(0,10)
+aleatório_5 = randint(0,10)
+tupla = (aleatório_1,aleatório_2,aleatório_3,aleatório_4,aleatório_5)
+print(f'Os valores sorteados foram: {tupla}')
+print(f'O menor valor gerado foi: {sorted(tupla)[0]}')
+print(f'O maior valor gerado foi: {sorted(tupla)[-1]}')
