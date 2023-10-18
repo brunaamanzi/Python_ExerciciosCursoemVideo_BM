ea61fb8e3023b1874e43e507a841e4f16fd7be4a
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-10-18 14:54:54 -0300

    Inclusão de novas resoluções

--- ex082.py
+++ ex082.py
@@ -1,1 +1,23 @@
-# Exercício 82:
+# Exercício 82: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
+lista_total = []
+lista_pares = []
+lista_impares = []
+while True:
+    num = int(input('Insira um número: '))
+    lista_total.append(num)
+    while True:
+        continua = input('Deseja continuar? [S/N] ').lower().strip()[0]
+        if continua in 'sn':
+            break
+    if continua == 'n':
+        break
+for valor in lista_total:
+    if valor % 2 == 0:
+        lista_pares.append(valor)
+    else:
+        lista_impares.append(valor)
+print('-'*40)
+print(f'A lista completa é {lista_total}')
+print(f'A lista que contém os números pares é: {lista_pares}')
+print(f'A lista que contém os números ímpares é: {lista_impares}')
+print('-'*40)
--- ex083.py
+++ ex083.py
@@ -1,1 +1,11 @@
-# Exercício 83:
+# Exercício 83: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+lista = []
+expressão = str(input('Digite sua expressão: '))
+abrindo = expressão.count('(')
+fechando = expressão.count(')')
+lista.append(abrindo)
+lista.append(fechando)
+if lista[0] == lista[1]:
+    print('Sua expressão está correta!')
+else:
+    print('Sua expressão está errada.')
