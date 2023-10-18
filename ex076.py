c2a14f9ede2a146c34f9e8d7d7177624070173f1
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-10-18 10:02:37 -0300

    Inclusão de novas resoluções

--- ex076.py
+++ ex076.py
@@ -2,8 +2,13 @@
 print('='*40)
 print(f'{"LISTAGEM DE PREÇOS":^40}')
 print('='*40)
-produtos = ('Lápis',1.75,'Borracha',2.00,'Caderno',15.90,'Estojo',25.00,'Transferidor',4.20,'Compasso',9.99,'Mochila',120.32,'Canetas',22.30,'Livro',34.90)
-print(f'{produtos[0]:.<30}R${produtos[1]:>8}')
+produtos = ('Lápis',1.75,'Borracha',2,'Caderno',15.90,'Estojo',25,'Transferidor',4.20,'Compasso',9.99,'Mochila',120.32,'Canetas',22.30,'Livro',34.90)
+for c in range (0,len(produtos)): # não podemos colocar for c in produtos, pois nesse caso tem que ser RANGE
+    if c % 2 == 0: # para verificar as posições pares (produtos)
+        print(f'{produtos[c]:.<30}',end='')
+    else: # para verificar posições ímpares (preços)
+        print(f'R${produtos[c]:>8.2f}')
+"""print(f'{produtos[0]:.<30}R${produtos[1]:>8}')
 print(f'{produtos[2]:.<30}R${produtos[3]:>8}')
 print(f'{produtos[4]:.<30}R${produtos[5]:>8}')
 print(f'{produtos[6]:.<30}R${produtos[7]:>8}')
@@ -11,6 +16,6 @@ print(f'{produtos[8]:.<30}R${produtos[9]:>8}')
 print(f'{produtos[10]:.<30}R${produtos[11]:>8}')
 print(f'{produtos[12]:.<30}R${produtos[13]:>8}')
 print(f'{produtos[14]:.<30}R${produtos[15]:>8}')
-print(f'{produtos[16]:.<30}R${produtos[17]:>8}')
+print(f'{produtos[16]:.<30}R${produtos[17]:>8}')"""
 print('='*40)
 
