b69497d52b8be14808ca6c535ffba6d01f2e9a46
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-10-18 14:56:45 -0300

    Inclusão de novas resoluções

--- ex078.py
+++ ex078.py
@@ -3,12 +3,12 @@ lista = []
 print('Olá! Por gentileza, escolha 5 números:')
 for c in range (0,5):
     lista.append(int(input(f'Insira um número na posição {c}: ')))
-print(f'Você digitou os valores {lista}')
-print(f'O maior valor digitado foi {max(lista)} e sua posição na lista é: ', end='')
+print(f'Você digitou os valores \033[1;32m{lista}\033[m')
+print(f'O maior valor digitado foi \033[1;32m{max(lista)}\033[m e sua posição na lista é: ', end='')
 for i, c in enumerate(lista):
     if c == max(lista):
         print(f'{i}, ',end='')
-print(f'\nO menor valor digitado foi {min(lista)} e sua posição na lista é: ', end='')
+print(f'\nO menor valor digitado foi \033[1;32m{min(lista)}\033[m e sua posição na lista é: ', end='')
 for i, c in enumerate(lista):
     if c == min(lista):
         print(f'{i}, ',end='')
