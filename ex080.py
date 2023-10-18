d3e2f9b74e7a964d50a7ec872450d62f9719310f
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-10-18 14:35:08 -0300

    Inclusão de novas resoluções

--- ex080.py
+++ ex080.py
@@ -1,15 +1,15 @@
 # Exercício 80: Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
 lista = []
+print('Olá! Por favor, pense em cinco números e iremos ordená-los para você.')
 for c in range (0,5):
     num = int(input('Insira um número: '))
     if len(lista) == 0 or num > lista[-1]:
         lista.append(num)
-        print(f'Número {num} adicionado na última posição.')
+        print(f'O número {num} foi inserido ao final da lista.')
     else:
-        for i, valor in enumerate(lista):
-            if num < valor:
+        for i,valor in enumerate(lista):
+            if num <= valor:
                 lista.insert(i,num)
-                print(f'Número {num} adicionado na posição {i}.')
-                break # utilizado para reduzir tempo de processamento
-print(lista)
-
+                print(f'O número {num} foi inserido na posição {i}.')
+                break
+print(f'A lista ordenada é: {lista}')
--- ex081.py
+++ ex081.py
@@ -1,1 +1,24 @@
-# Exercício 81:
+"""Exercício 81: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre:
+a) Quantos números foram digitados;
+b) A lista de valores, ordenada de forma decrescente;
+c) Se o valor 5 foi digitado e está ou não na lista."""
+lista = []
+while True:
+    num = int(input('Insira um número: '))
+    lista.append(num)
+    while True:
+        continua = input('Deseja continuar? [S/N] ').lower().strip()[0]
+        if continua in 'sn':
+            break
+    if continua == 'n':
+        break
+print('='*40)
+print(f'Lista digitada: {lista}')
+print(f'A lista possui {len(lista)} números.')
+lista.sort(reverse=True)
+print(f'Lista ordenada de forma decrescente: {lista}') # Não conseguimos imprimir a lista já ordenando pois primeiro precisamos ordenar para depois imprimir o resultado nesse caso.
+if 5 in lista:
+    print(f'O valor 5 foi digitado na posição {lista.index(5)}')
+else:
+    print('O valor 5 não está na lista.')
+print('='*40)
