be8ab991d4e17f82754896666bd52046c3f3dd23
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-10-19 20:36:55 -0300

    Inclusão de novas resoluções

--- ex085.py
+++ ex085.py
@@ -1,7 +1,7 @@
 # Exercício 85: Crie um programa onde o usuário possa digitar 7 valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 numeros = [[],[]]
-for c in range (0,7):
-    num = int(input('Insira um número: '))
+for c in range (1,8):
+    num = int(input(f'Insira o {c}º número: '))
     if num % 2 == 0:
         numeros[0].append(num)
     else:
