e7cc1f8db52dcb61de3ac670a18b8be5bffc9290
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-10-20 12:12:33 -0300

    Inclusão de novas resoluções

--- ex091.py
+++ ex091.py
@@ -1,19 +1,35 @@
 # Exercício 91: Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+from time import sleep
 from random import randint
 from operator import itemgetter
-resultados = {}
-resultados['jogador1'] = randint(0,6)
-resultados['jogador2'] = randint(0,6)
-resultados['jogador3'] = randint(0,6)
-resultados['jogador4'] = randint(0,6)
+jogo = {}
+ranking = {}
+# Também poderia colocar tudo direto dentro do dicionário:
+"""jogo={'jogador1': randint(0,6),
+            'jogador2': randint(0,6),
+            'jogador3': randint(0,6),
+            'jogador4': randint(0,6)}"""
+jogo['jogador1'] = randint(0,6)
+jogo['jogador2'] = randint(0,6)
+jogo['jogador3'] = randint(0,6)
+jogo['jogador4'] = randint(0,6)
 print('-'*30)
 print(f'{"Valores sorteados:":^30} ')
 print('-'*30)
-for k,v in resultados(items):
+sleep(0.5)
+for k,v in jogo.items():
     print(f'{k} tirou {v} no dado.')
-print('*'*30)
+    sleep(1)
+print('-'*30)
 print(f'{"RANKING DOS JOGADORES":^30}')
 print('-'*30)
+sleep(0.5)
+ranking = sorted(jogo.items(),key=itemgetter(1),reverse=True) # ao utilizar a função sorted, ela transforma em lista
+for i, v in enumerate(ranking):
+    print(f'{i+1}º Lugar: {v[0]} com {v[1]} pontos.')
+    sleep(1)
+
+
 
 
 
--- ex092.py
+++ ex092.py
@@ -1,16 +1,17 @@
 # Exercício 92: Crie um programa que leia: nome, ano de nascimento e carteira de trabalho, e cadastre-os (com idade) em um dicionário. Se por acaso a CTPS for diferente de zero, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
 # Considera que para se aposentar, é necessário ter 35 anos de contribuição.
+from datetime import date
 dados = dict()
 dados['nome']=input('Nome: ')
 ano_nascimento = int(input('Ano de Nascimento: '))
-from datetime import date
 ano_atual = date.today().year
 dados['idade']=ano_atual-ano_nascimento
 dados['ctps']=int(input('Carteira de Trabalho (0 não tem): '))
 if dados['ctps'] != 0:
     dados['contratação']=int(input('Ano de contratação: '))
     dados['salário']=float(input('Salário: R$ '))
-    dados['aposentadoria']=ano_atual-dados['contratação']
-print(dados)
+    dados['aposentadoria']=dados["idade"]+((dados['contratação']+35)-ano_atual)
+print('-'*30)
 for k,v in dados.items():
     print(f'{k} tem o valor {v}')
+print('-'*30)
