c5031bdac3820bcf850f34b8a3e524482eda201c
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-10-21 12:15:06 -0300

    Inclusão de novas resoluções

--- ex098.py
+++ ex098.py
@@ -3,7 +3,7 @@ Seu programa tem que realizar três contagens através da função criada:
 a) De 1 até 10, de 1 em 1
 b) De 10 até 0, de 2 em 2
 c) Uma contagem personalizada"""
-from time import sleep()
+from time import sleep
 def lin():
     print('-='*20)
 lin()
@@ -23,11 +23,16 @@ def contador(f,i,p):
     print('FIM!')
 contador(10,-1,-2)
 lin()
-def contador(f,i,p):
-    print(f'De {f} a {i}, de {p} em {p}:')
-    for c in range(f,i,p):
-        print(c,end=' ')
+def contador(i,f,p):
+    print(f'De {i} a {f}, de {p} em {p}:')
+    for c in range(i, f + 1, p):
+        print(c, end=' ')
         sleep(0.5)
     print('FIM!')
-contador(50,-1,-2)
+print('Agora é a sua vez de personalizar a contagem!')
+i = int(input('Início: '))
+f = int(input('Fim:    '))
+p = int(input('Passo:  '))
+contador(i,f,p)
+
 
