9b19f167a46d886b9bb27d6d86462f1503c45aa4
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-10-23 19:30:44 -0300

    Inclusão de novas resoluções

---
+++ ex103b.py
@@ -0,0 +1,12 @@
+# Outra opção para o exercício 103:
+def ficha(n='', g=0):
+    try:
+        n = input('Nome do jogador: ')
+        g = int(input('Número de gols: '))
+    except ValueError:
+        n = '<desconhecido>'
+        g = 0
+    print(f'O jogador {n} fez {g} gol(s).')
+
+
+ficha()
