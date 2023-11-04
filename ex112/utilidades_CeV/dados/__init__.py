f0d7e4e24eda2bc1dc0b8872f52631a840b03408
brunamanzi <brunaamanzi@gmail.com>
2023-11-04 16:00:13 -0300

    Inclusão de novas resoluções

--- ex112/utilidades_CeV/dados/__init__.py
+++ ex112/utilidades_CeV/dados/__init__.py
@@ -1,1 +1,8 @@
-def leiaDinheiro():
+def leiaDinheiro(num):
+        while True:
+            num = float(input("Digite o preço: R$"))
+            return num
+            except ValueError:
+                print(f'\033[1;31mERRO! "{num}" é um preço inválido!\033[m')
+            if num.isnumeric():
+                break
