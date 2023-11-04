5769ce9ce2c1adbac7c455f4dbe188a154d11f28
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-11-04 10:32:13 -0300

    Inclusão de novas resoluções

--- ex110/exercício.py
+++ ex110/exercício.py
@@ -1,4 +1,4 @@
-"""Desafio 109: Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui."""
+"""Desafio 110: Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui."""
 
 from ex110 import moeda
 
