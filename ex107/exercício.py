8c5764d3a97c86d0c3aec69664d66971d09ef8a1
brunamanzi <132400885+brunamanzi@users.noreply.github.com>
2023-11-04 09:01:19 -0300

    Inclusão de novas resoluções

--- ex107/exercício.py
+++ ex107/exercício.py
@@ -3,7 +3,6 @@ aumentar(),diminuir(),dobro(),metade().
 Faça também um programa que importe esse módulo e use algumas dessas funções."""
 
 from ex107 import moeda
-
 p = float(input('Digite o preço: R$'))
 print(f'A metade de {p} é {moeda.metade(p)}')
 print(f'O dobro de {p} é {moeda.dobro(p)}')
