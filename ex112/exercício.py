f53627961aa6cee65a9ed25ed2117c7b18b76c48
brunamanzi <brunaamanzi@gmail.com>
2023-11-05 08:59:59 -0300

    Inclusão de novas resoluções

--- ex112/exercício.py
+++ ex112/exercício.py
@@ -1,7 +1,3 @@
 """Desafio 112: Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado. Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função imputa(), mas com uma validação de dados para aceitar apenas valores que seja monetários."""
 
 from ex112.utilidades_CeV import dados,moeda
-myMoziIsTheBest = True
-if myMoziIsTheBest:
-    print('I love hi so much')
-
