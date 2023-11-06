352e7873ed4e5c57c62261f02f931014c9404fd2
brunamanzi <brunaamanzi@gmail.com>
2023-11-06 20:55:21 -0300

    Inclusão de novas resoluções

--- ex114.py
+++ ex114.py
@@ -1,2 +1,10 @@
 # Exercício 114: Crie um código em Python que teste se o site pudim está acessível pelo computador usado.
+import urllib
+import urllib.request
 
+try:
+    site = urllib.request.urlopen('https://www.google.com.br/')
+except urllib.error.URLError: # poderia ser apenas except:
+    print('Deu erro!')
+else:
+    print('Tudo ok')
